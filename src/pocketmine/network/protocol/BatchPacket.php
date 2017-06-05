a3c70b6fa7fa21e902236a62eefa517002c45196
LinuxLogo <rodjon97@outlook.com>
2017-06-05 13:43:55 -0400

    Update BatchPacket.php

--- src/pocketmine/network/protocol/BatchPacket.php
+++ src/pocketmine/network/protocol/BatchPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,20 +15,32 @@
  *
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
- * 
+ *
  *
 */
 
-namespace pocketmine\network\protocol;
+namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
 
-class BatchPacket extends DataPacket{
+use pocketmine\network\mcpe\NetworkSession;
+use pocketmine\utils\Binary;
+use pocketmine\utils\BinaryStream;
 
+class BatchPacket extends DataPacket{
 	const NETWORK_ID = 0xfe;
 
 	public $payload;
+	public $compressed = false;
+
+	public function canBeBatched() : bool{
+		return false;
+	}
+
+	public function canBeSentBeforeLogin() : bool{
+		return true;
+	}
 
 	public function decode(){
 		$this->payload = $this->get(true);
@@ -36,14 +48,58 @@ class BatchPacket extends DataPacket{
 
 	public function encode(){
 		$this->reset();
+		assert($this->compressed);
 		$this->put($this->payload);
 	}
 
 	/**
-	 * @return PacketName|string
-     */
-	public function getName(){
-		return "BatchPacket";
+	 * @param DataPacket|string $packet
+	 */
+	public function addPacket($packet){
+		if($packet instanceof DataPacket){
+			if(!$packet->isEncoded){
+				$packet->encode();
+			}
+			$packet = $packet->buffer;
+		}
+
+		$this->payload .= Binary::writeUnsignedVarInt(strlen($packet)) . $packet;
 	}
 
-}
+	public function compress(int $level = 7){
+		assert(!$this->compressed);
+		$this->payload = zlib_encode($this->payload, ZLIB_ENCODING_DEFLATE, $level);
+		$this->compressed = true;
+	}
+
+	public function handle(NetworkSession $session) : bool{
+		if(strlen($this->payload) < 2){
+			return false;
+		}
+
+		try{
+			$str = zlib_decode($this->payload, 1024 * 1024 * 64); //Max 64MB
+		}catch(\ErrorException $e){
+			return false;
+		}
+
+		if(strlen($str) === 0){
+			throw new \InvalidStateException("Decoded BatchPacket payload is empty");
+		}
+
+		$this->setBuffer($str, 0);
+
+		$network = $session->getServer()->getNetwork();
+		while(!$this->feof()){
+			$buf = $this->getString();
+			$pk = $network->getPacket(ord($buf{0}));
+			/*if(!$pk->canBeBatched()){
+				throw new \InvalidArgumentException("Received invalid " . get_class($pk) . " inside BatchPacket");
+			}*/
+
+			$pk->setBuffer($buf, 1);
+			$session->handleDataPacket($pk);
+		}
+
+		return true;
+	}
